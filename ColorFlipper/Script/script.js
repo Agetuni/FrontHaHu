a42ed204cb6ee3983363b828118b39ab1df8b3d3
Aleazar yilma <yilma4060@gmail.com>
2022-06-04 19:35:39 +0300

    Add color types
    color list in script.js are modified

--- ColorFlipper/Script/script.js
+++ ColorFlipper/Script/script.js
@@ -1,4 +1,4 @@
-const colors = ["yellow", "green", "blue", "tomatoo", "aqua"];
+const colors = ["Tomato", "Orange", "DodgerBlue", "MediumSeaGreen","Grey", "SlateBlue","Violet","LightGray"];
 const hexas = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 // change colors from color
 function changeColor() {
